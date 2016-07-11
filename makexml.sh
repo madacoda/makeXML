45f4ed019d808b355f39ed74e63302f42ce7f0fa
webdevalto <madatoman@gmail.com>
2016-07-11 11:48:58 -0400

    Initial Commit

---
+++ makexml.sh
@@ -0,0 +1,3 @@
+echo "<object-field name=\"Radio\" display-name=\"Radio\" type=\"text\" object-name=\"ip4-block\" required=\"false\" predefined-values=\"">xmltest.xml
+for i in {1..1000};do echo -n "unit"$i"|">>xmltest.xml;done
+echo -n "\" render-type=\"radio\"/>" >> xmltest.xml
